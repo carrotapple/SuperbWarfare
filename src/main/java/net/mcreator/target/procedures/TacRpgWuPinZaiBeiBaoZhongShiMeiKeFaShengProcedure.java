c6c82d1f1ba3b920939d52d23ef76445e887c4e4
17146 <1714673995@qq.com>
2024-05-10 01:06:32 +0800

    规范化音效名称

--- src/main/java/net/mcreator/target/procedures/TacRpgWuPinZaiBeiBaoZhongShiMeiKeFaShengProcedure.java
+++ src/main/java/net/mcreator/target/procedures/TacRpgWuPinZaiBeiBaoZhongShiMeiKeFaShengProcedure.java
@@ -29,7 +29,7 @@ public class TacRpgWuPinZaiBeiBaoZhongShiMeiKeFaShengProcedure {
                     Entity _ent = entity;
                     if (!_ent.level().isClientSide() && _ent.getServer() != null) {
                         _ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-                                _ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "playsound target:rpg7_reload player @s ~ ~ ~ 100 1");
+                                _ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "playsound target:rpg_reload player @s ~ ~ ~ 100 1");
                     }
                 }
             }
