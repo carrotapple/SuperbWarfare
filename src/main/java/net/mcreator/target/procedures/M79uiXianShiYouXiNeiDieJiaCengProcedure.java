7820caf7bc9c6a1bf5b67c31af1a0722d12eea34
Light_Quanta <zch22248@foxmail.com>
2024-05-09 21:52:20 +0800

    使准心在旁观者模式下隐藏

--- src/main/java/net/mcreator/target/procedures/M79uiXianShiYouXiNeiDieJiaCengProcedure.java
+++ src/main/java/net/mcreator/target/procedures/M79uiXianShiYouXiNeiDieJiaCengProcedure.java
@@ -5,14 +5,17 @@ import net.mcreator.target.network.TargetModVariables;
 import net.minecraft.client.CameraType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 
 public class M79uiXianShiYouXiNeiDieJiaCengProcedure {
     public static boolean execute(Entity entity) {
-        if (entity == null)
-            return false;
-        return (entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == TargetModItems.M_79.get() && Minecraft.getInstance().options.getCameraType() == CameraType.FIRST_PERSON
-                && !(entity.getCapability(TargetModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new TargetModVariables.PlayerVariables())).zooming;
+        if (entity == null) return false;
+        if (entity instanceof Player player) {
+            return !player.isSpectator()
+                    && player.getMainHandItem().getItem() == TargetModItems.M_79.get()
+                    && Minecraft.getInstance().options.getCameraType() == CameraType.FIRST_PERSON
+                    && !entity.getCapability(TargetModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new TargetModVariables.PlayerVariables()).zooming;
+        }
+        return false;
     }
 }
