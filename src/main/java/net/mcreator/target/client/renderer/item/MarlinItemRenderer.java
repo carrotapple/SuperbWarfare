9afbc97ab5a9e34bea53e4d5d0696da9b91ba347
Atsuihsio <842960157@qq.com>
2024-05-12 14:29:50 +0800

    修复M870和M1895不播放动画的bug

--- src/main/java/net/mcreator/target/client/renderer/item/MarlinItemRenderer.java
+++ src/main/java/net/mcreator/target/client/renderer/item/MarlinItemRenderer.java
@@ -1,15 +1,59 @@
 package net.mcreator.target.client.renderer.item;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.mcreator.target.item.gun.MarlinItem;
 import net.mcreator.target.client.layer.MarlinLayer;
 import net.mcreator.target.client.model.item.MarlinItemModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class MarlinItemRenderer extends GeoItemRenderer<MarlinItem> {
     public MarlinItemRenderer() {
         super(new MarlinItemModel());
         this.addRenderLayer(new MarlinLayer(this));
+    }
+
+    @Override
+    public RenderType getRenderType(MarlinItem animatable, ResourceLocation texture, MultiBufferSource bufferSource, float partialTick) {
+        return RenderType.entityTranslucent(getTextureLocation(animatable));
+    }
+
+    private static final float SCALE_RECIPROCAL = 1.0f / 16.0f;
+    protected boolean renderArms = false;
+    protected MultiBufferSource currentBuffer;
+    protected RenderType renderType;
+    public ItemDisplayContext transformType;
+    protected MarlinItem animatable;
+    private final Set<String> hiddenBones = new HashSet<>();
+    private final Set<String> suppressedBones = new HashSet<>();
+
+    @Override
+    public void renderByItem(ItemStack stack, ItemDisplayContext transformType, PoseStack matrixStack, MultiBufferSource bufferIn, int combinedLightIn, int p_239207_6_) {
+        this.transformType = transformType;
+        if (this.animatable != null)
+            this.animatable.getTransformType(transformType);
+        super.renderByItem(stack, transformType, matrixStack, bufferIn, combinedLightIn, p_239207_6_);
+    }
+
+    @Override
+    public void actuallyRender(PoseStack matrixStackIn, MarlinItem animatable, BakedGeoModel model, RenderType type, MultiBufferSource renderTypeBuffer, VertexConsumer vertexBuilder, boolean isRenderer, float partialTicks, int packedLightIn,
+                               int packedOverlayIn, float red, float green, float blue, float alpha) {
+        this.currentBuffer = renderTypeBuffer;
+        this.renderType = type;
+        this.animatable = animatable;
+        super.actuallyRender(matrixStackIn, animatable, model, type, renderTypeBuffer, vertexBuilder, isRenderer, partialTicks, packedLightIn, packedOverlayIn, red, green, blue, alpha);
+        if (this.renderArms) {
+            this.renderArms = false;
+        }
     }
 
     @Override
