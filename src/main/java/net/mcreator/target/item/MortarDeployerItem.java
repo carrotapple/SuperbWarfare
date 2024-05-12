bd716ecdeb54733ae840e7edd4ccfee9572873f6
Light_Quanta <zch22248@foxmail.com>
2024-05-12 16:47:24 +0800

    内联迫击炮部署过程

--- src/main/java/net/mcreator/target/item/MortarDeployerItem.java
+++ src/main/java/net/mcreator/target/item/MortarDeployerItem.java
@@ -1,9 +1,13 @@
 package net.mcreator.target.item;
 
-import net.mcreator.target.procedures.RengchumortarProcedure;
+import net.mcreator.target.init.TargetModEntities;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -24,9 +28,24 @@ public class MortarDeployerItem extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level world, Player entity, InteractionHand hand) {
-        InteractionResultHolder<ItemStack> ar = super.use(world, entity, hand);
-        RengchumortarProcedure.execute(world, entity.getX(), entity.getY(), entity.getZ(), entity);
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        InteractionResultHolder<ItemStack> ar = super.use(world, player, hand);
+
+        if (world instanceof ServerLevel level) {
+            Entity entityToSpawn = TargetModEntities.MORTAR.get().spawn(level, BlockPos.containing(player.getX() + 1.5 * player.getLookAngle().x, player.getY(), player.getZ() + 1.5 * player.getLookAngle().z), MobSpawnType.MOB_SUMMONED);
+            if (entityToSpawn != null) {
+                entityToSpawn.setYRot(player.getYRot());
+                entityToSpawn.setYBodyRot(player.getYRot());
+                entityToSpawn.setYHeadRot(player.getYRot());
+                entityToSpawn.setXRot(-70);
+                entityToSpawn.setDeltaMovement(0, 0, 0);
+            }
+        }
+        if (!player.isCreative()) {
+            player.getItemInHand(hand).shrink(1);
+            player.swing(InteractionHand.MAIN_HAND, true);
+        }
+
         return ar;
     }
 }
