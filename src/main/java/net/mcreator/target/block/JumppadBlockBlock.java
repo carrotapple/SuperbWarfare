e05b946bcc62793d01dde29233b3a09d99efcd71
Light_Quanta <zch22248@foxmail.com>
2024-05-15 23:55:51 +0800

    内联跳板弹射过程

--- src/main/java/net/mcreator/target/block/JumppadBlockBlock.java
+++ src/main/java/net/mcreator/target/block/JumppadBlockBlock.java
@@ -1,9 +1,12 @@
 package net.mcreator.target.block;
 
-import net.mcreator.target.procedures.JumppadBlockShiTiZaiFangKuaiZhongPengZhuangShiProcedure;
+import net.mcreator.target.entity.Target1Entity;
+import net.mcreator.target.init.TargetModSounds;
+import net.mcreator.target.network.TargetModVariables;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -17,6 +20,7 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -93,8 +97,35 @@ public class JumppadBlockBlock extends Block {
     }
 
     @Override
-    public void entityInside(BlockState blockstate, Level world, BlockPos pos, Entity entity) {
-        super.entityInside(blockstate, world, pos, entity);
-        JumppadBlockShiTiZaiFangKuaiZhongPengZhuangShiProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ(), entity);
+    public void entityInside(BlockState blockstate, Level level, BlockPos pos, Entity entity) {
+        super.entityInside(blockstate, level, pos, entity);
+
+        // 禁止套娃
+        if (entity instanceof Target1Entity) return;
+
+        boolean zooming = entity.getCapability(TargetModVariables.PLAYER_VARIABLES_CAPABILITY, null).map(c -> c.zooming).orElse(false);
+
+        if (entity.isShiftKeyDown()) {
+            if (entity.onGround()) {
+                entity.setDeltaMovement(new Vec3(5 * entity.getLookAngle().x, 1.5, 5 * entity.getLookAngle().z));
+            } else {
+                entity.setDeltaMovement(new Vec3(1.8 * entity.getLookAngle().x, 1.5, 1.8 * entity.getLookAngle().z));
+            }
+        } else {
+            entity.setDeltaMovement(new Vec3(0.7 * entity.getDeltaMovement().x(), 1.7, 0.7 * entity.getDeltaMovement().z()));
+        }
+
+        if (!zooming) entity.getPersistentData().putDouble("vy", 0.8);
+
+        if (!level.isClientSide()) {
+            level.playSound(null, BlockPos.containing(pos.getX(), pos.getY(), pos.getZ()), TargetModSounds.JUMP.get(), SoundSource.BLOCKS, 1, 1);
+        } else {
+            level.playLocalSound(pos.getX(), pos.getY(), pos.getZ(), TargetModSounds.JUMP.get(), SoundSource.BLOCKS, 1, 1, false);
+        }
+
+        entity.getCapability(TargetModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+            capability.playerDoubleJump = true;
+            capability.syncPlayerVariables(entity);
+        });
     }
 }
