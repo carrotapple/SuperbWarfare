e708b11b94862166b533cd201fc46f8ddbaec7a5
Atsuihsio <842960157@qq.com>
2024-05-16 15:18:18 +0800

    修复迫击炮弹不爆炸，添加又是一个小细节.jpg

--- src/main/java/net/mcreator/target/entity/MortarShellEntity.java
+++ src/main/java/net/mcreator/target/entity/MortarShellEntity.java
@@ -85,11 +85,8 @@ public class MortarShellEntity extends AbstractArrow implements ItemSupplier {
     @Override
     public void onHitBlock(BlockHitResult blockHitResult) {
         super.onHitBlock(blockHitResult);
-
-        if (this.getPersistentData().getDouble("time") > 0) {
             if (this.level() instanceof ServerLevel level) {
                 level.explode(this, this.getX(), this.getY(), this.getZ(), 10, Level.ExplosionInteraction.NONE);
-            }
         }
     }
 
@@ -108,36 +105,5 @@ public class MortarShellEntity extends AbstractArrow implements ItemSupplier {
             }
             this.discard();
         }
-    }
-
-    public static MortarShellEntity shoot(Level world, LivingEntity entity, RandomSource source) {
-        return shoot(world, entity, source, 1f, 5, 5);
-    }
-
-    public static MortarShellEntity shoot(Level world, LivingEntity entity, RandomSource random, float power, double damage, int knockback) {
-        MortarShellEntity entityarrow = new MortarShellEntity(TargetModEntities.MORTAR_SHELL.get(), entity, world);
-        entityarrow.shoot(entity.getViewVector(1).x, entity.getViewVector(1).y, entity.getViewVector(1).z, power * 2, 0);
-        entityarrow.setSilent(true);
-        entityarrow.setCritArrow(false);
-        entityarrow.setBaseDamage(damage);
-        entityarrow.setKnockback(knockback);
-        world.addFreshEntity(entityarrow);
-        world.playSound(null, entity.getX(), entity.getY(), entity.getZ(), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.arrow.shoot")), SoundSource.PLAYERS, 1, 1f / (random.nextFloat() * 0.5f + 1) + (power / 2));
-        return entityarrow;
-    }
-
-    public static MortarShellEntity shoot(LivingEntity entity, LivingEntity target) {
-        MortarShellEntity entityarrow = new MortarShellEntity(TargetModEntities.MORTAR_SHELL.get(), entity, entity.level());
-        double dx = target.getX() - entity.getX();
-        double dy = target.getY() + target.getEyeHeight() - 1.1;
-        double dz = target.getZ() - entity.getZ();
-        entityarrow.shoot(dx, dy - entityarrow.getY() + Math.hypot(dx, dz) * 0.2F, dz, 1f * 2, 12.0F);
-        entityarrow.setSilent(true);
-        entityarrow.setBaseDamage(5);
-        entityarrow.setKnockback(5);
-        entityarrow.setCritArrow(false);
-        entity.level().addFreshEntity(entityarrow);
-        entity.level().playSound(null, entity.getX(), entity.getY(), entity.getZ(), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.arrow.shoot")), SoundSource.PLAYERS, 1, 1f / (RandomSource.create().nextFloat() * 0.5f + 1));
-        return entityarrow;
     }
 }
