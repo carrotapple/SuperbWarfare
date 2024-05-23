f127dc79980387410a68abd336a8a13fb45b4948
Atsuihsio <842960157@qq.com>
2024-05-23 14:46:05 +0800

    动画更新(M79)

--- src/main/java/net/mcreator/target/client/model/item/M79ItemModel.java
+++ src/main/java/net/mcreator/target/client/model/item/M79ItemModel.java
@@ -4,6 +4,7 @@ import net.mcreator.target.item.gun.M79Item;
 import net.mcreator.target.network.TargetModVariables;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
@@ -114,5 +115,13 @@ public class M79ItemModel extends GeoModel<M79Item> {
         move.setRotZ(3.7f * (float) yaw + 2.7f * (float) m);
 
         move.setRotY(1.9f * (float) yaw - 1.7f * (float) m);
+
+        CoreGeoBone camera = getAnimationProcessor().getBone("camera");
+
+        player.getPersistentData().putDouble("camera_rot_x", Mth.RAD_TO_DEG * camera.getRotX());
+
+        player.getPersistentData().putDouble("camera_rot_y", Mth.RAD_TO_DEG * camera.getRotY());
+
+        player.getPersistentData().putDouble("camera_rot_z", Mth.RAD_TO_DEG * camera.getRotZ());
     }
 }
