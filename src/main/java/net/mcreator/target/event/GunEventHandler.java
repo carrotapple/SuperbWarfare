4dfcffc9bd12582bb188bfca1b3bcd09e1681eed
Atsuihsio <842960157@qq.com>
2024-06-05 16:16:14 +0800

    调整发射角度，武器数据

--- src/main/java/net/mcreator/target/event/GunEventHandler.java
+++ src/main/java/net/mcreator/target/event/GunEventHandler.java
@@ -341,8 +341,8 @@ public class GunEventHandler {
                 projectile.beast();
             }
 
-            projectile.setPos(player.getX() - 0.1 * player.getLookAngle().x, player.getEyeY() - 0.07 - 0.1 * player.getLookAngle().y, player.getZ() + -0.1 * player.getLookAngle().z);
-            projectile.shoot(player.getLookAngle().x, player.getLookAngle().y + 0.003f , player.getLookAngle().z, 1 * (float) heldItem.getOrCreateTag().getDouble("velocity"),
+            projectile.setPos(player.getX() - 0.1 * player.getLookAngle().x, player.getEyeY() - 0.1 - 0.1 * player.getLookAngle().y, player.getZ() + -0.1 * player.getLookAngle().z);
+            projectile.shoot(player.getLookAngle().x, player.getLookAngle().y + 0.001f , player.getLookAngle().z, 1 * (float) heldItem.getOrCreateTag().getDouble("velocity"),
                     (float) player.getAttributeBaseValue(TargetModAttributes.SPREAD.get()));
             player.level().addFreshEntity(projectile);
         }
