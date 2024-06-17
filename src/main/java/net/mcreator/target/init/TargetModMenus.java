0a8f3662ba62fab7f13e512a3806476b9463a666
17146 <1714673995@qq.com>
2024-06-18 00:59:35 +0800

    删除冗余menu类

--- src/main/java/net/mcreator/target/init/TargetModMenus.java
+++ src/main/java/net/mcreator/target/init/TargetModMenus.java
@@ -2,7 +2,6 @@ package net.mcreator.target.init;
 
 import net.mcreator.target.TargetMod;
 import net.mcreator.target.world.inventory.GunRecycleGuiMenu;
-import net.mcreator.target.world.inventory.MortarGUIMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,6 +11,5 @@ import net.minecraftforge.registries.RegistryObject;
 public class TargetModMenus {
     public static final DeferredRegister<MenuType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.MENU_TYPES, TargetMod.MODID);
 
-    public static final RegistryObject<MenuType<MortarGUIMenu>> MORTAR_GUI = REGISTRY.register("mortar_gui", () -> IForgeMenuType.create(MortarGUIMenu::new));
     public static final RegistryObject<MenuType<GunRecycleGuiMenu>> GUN_RECYCLE_GUI = REGISTRY.register("gun_recycle_gui", () -> IForgeMenuType.create(GunRecycleGuiMenu::new));
 }
