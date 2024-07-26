1c90597c5caf5955db3f7163aea2b0a25fca8f2a
Atsuihsio <842960157@qq.com>
2024-07-27 03:17:56 +0800

    调整无人机血量

--- src/main/java/net/mcreator/superbwarfare/entity/DroneEntity.java
+++ src/main/java/net/mcreator/superbwarfare/entity/DroneEntity.java
@@ -403,7 +403,7 @@ public class DroneEntity extends PathfinderMob implements GeoEntity {
     public static AttributeSupplier.Builder createAttributes() {
         AttributeSupplier.Builder builder = Mob.createMobAttributes();
         builder = builder.add(Attributes.MOVEMENT_SPEED, 0.1);
-        builder = builder.add(Attributes.MAX_HEALTH, 20);
+        builder = builder.add(Attributes.MAX_HEALTH, 4);
         builder = builder.add(Attributes.ARMOR, 0);
         builder = builder.add(Attributes.ATTACK_DAMAGE, 0);
         builder = builder.add(Attributes.FOLLOW_RANGE, 64);
