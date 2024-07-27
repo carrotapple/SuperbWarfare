609559d84a67b069da0b12c68d8d2df638d468f9
Atsuihsio <842960157@qq.com>
2024-07-27 16:53:49 +0800

    修改放置器的放置逻辑

--- src/main/java/net/mcreator/superbwarfare/item/Drone.java
+++ src/main/java/net/mcreator/superbwarfare/item/Drone.java
@@ -1,17 +1,34 @@
 package net.mcreator.superbwarfare.item;
 
+import net.mcreator.superbwarfare.entity.DroneEntity;
 import net.mcreator.superbwarfare.init.ModEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+
+import java.util.Objects;
 
 public class Drone extends Item {
     public Drone() {
@@ -19,24 +36,59 @@ public class Drone extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
-        InteractionResultHolder<ItemStack> ar = super.use(world, player, hand);
+    public InteractionResult useOn(UseOnContext pContext) {
+        Level level = pContext.getLevel();
+        if (!(level instanceof ServerLevel)) {
+            return InteractionResult.SUCCESS;
+        } else {
+            ItemStack itemstack = pContext.getItemInHand();
+            BlockPos blockpos = pContext.getClickedPos();
+            Direction direction = pContext.getClickedFace();
+            BlockState blockstate = level.getBlockState(blockpos);
+            BlockPos blockpos1;
+            if (blockstate.getCollisionShape(level, blockpos).isEmpty()) {
+                blockpos1 = blockpos;
+            } else {
+                blockpos1 = blockpos.relative(direction);
+            }
 
-        if (world instanceof ServerLevel level) {
-            Entity entityToSpawn = ModEntities.DRONE.get().spawn(level, BlockPos.containing(player.getX() + 1.5 * player.getLookAngle().x, player.getY(), player.getZ() + 1.5 * player.getLookAngle().z), MobSpawnType.MOB_SUMMONED);
-            if (entityToSpawn != null) {
-                entityToSpawn.setYRot(player.getYRot());
-                entityToSpawn.setYBodyRot(player.getYRot());
-                entityToSpawn.setYHeadRot(player.getYRot());
-                entityToSpawn.setDeltaMovement(0, 0, 0);
+            if (ModEntities.DRONE.get().spawn((ServerLevel)level, itemstack, pContext.getPlayer(), blockpos1, MobSpawnType.SPAWN_EGG, true, !Objects.equals(blockpos, blockpos1) && direction == Direction.UP) != null) {
+                itemstack.shrink(1);
+                level.gameEvent(pContext.getPlayer(), GameEvent.ENTITY_PLACE, blockpos);
+            }
+
+            return InteractionResult.CONSUME;
+        }
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        BlockHitResult blockhitresult = getPlayerPOVHitResult(pLevel, pPlayer, ClipContext.Fluid.SOURCE_ONLY);
+        if (blockhitresult.getType() != HitResult.Type.BLOCK) {
+            return InteractionResultHolder.pass(itemstack);
+        } else if (!(pLevel instanceof ServerLevel)) {
+            return InteractionResultHolder.success(itemstack);
+        } else {
+            BlockPos blockpos = blockhitresult.getBlockPos();
+            if (!(pLevel.getBlockState(blockpos).getBlock() instanceof LiquidBlock)) {
+                return InteractionResultHolder.pass(itemstack);
+            } else if (pLevel.mayInteract(pPlayer, blockpos) && pPlayer.mayUseItemAt(blockpos, blockhitresult.getDirection(), itemstack)) {
+                Entity entity = ModEntities.DRONE.get().spawn((ServerLevel)pLevel, itemstack, pPlayer, blockpos, MobSpawnType.SPAWN_EGG, false, false);
+                if (entity == null) {
+                    return InteractionResultHolder.pass(itemstack);
+                } else {
+                    if (!pPlayer.getAbilities().instabuild) {
+                        itemstack.shrink(1);
+                    }
+
+                    pPlayer.awardStat(Stats.ITEM_USED.get(this));
+                    pLevel.gameEvent(pPlayer, GameEvent.ENTITY_PLACE, entity.position());
+                    return InteractionResultHolder.consume(itemstack);
+                }
+            } else {
+                return InteractionResultHolder.fail(itemstack);
             }
         }
-
-        if (!player.isCreative()) {
-            player.getItemInHand(hand).shrink(1);
-            player.swing(InteractionHand.MAIN_HAND, true);
-        }
-
-        return ar;
     }
 }
--- src/main/java/net/mcreator/superbwarfare/item/TargetDeployer.java
+++ src/main/java/net/mcreator/superbwarfare/item/TargetDeployer.java
@@ -2,13 +2,27 @@ package net.mcreator.superbwarfare.item;
 
 import net.mcreator.superbwarfare.init.ModEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+
+import java.util.Objects;
 
 public class TargetDeployer extends Item {
     public TargetDeployer() {
@@ -16,23 +30,59 @@ public class TargetDeployer extends Item {
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext context) {
-        super.useOn(context);
+    public InteractionResult useOn(UseOnContext pContext) {
+        Level level = pContext.getLevel();
+        if (!(level instanceof ServerLevel)) {
+            return InteractionResult.SUCCESS;
+        } else {
+            ItemStack itemstack = pContext.getItemInHand();
+            BlockPos blockpos = pContext.getClickedPos();
+            Direction direction = pContext.getClickedFace();
+            BlockState blockstate = level.getBlockState(blockpos);
+            BlockPos blockpos1;
+            if (blockstate.getCollisionShape(level, blockpos).isEmpty()) {
+                blockpos1 = blockpos;
+            } else {
+                blockpos1 = blockpos.relative(direction);
+            }
 
-        var clickedPos = context.getClickedPos();
-        var player = context.getPlayer();
-        if (player == null) return InteractionResult.FAIL;
+            if (ModEntities.TARGET_1.get().spawn((ServerLevel)level, itemstack, pContext.getPlayer(), blockpos1, MobSpawnType.SPAWN_EGG, true, !Objects.equals(blockpos, blockpos1) && direction == Direction.UP) != null) {
+                itemstack.shrink(1);
+                level.gameEvent(pContext.getPlayer(), GameEvent.ENTITY_PLACE, blockpos);
+            }
 
-        if (context.getLevel() instanceof ServerLevel level) {
-            Entity entityToSpawn = ModEntities.TARGET_1.get().spawn(level, BlockPos.containing(clickedPos.getX() + 0.5, clickedPos.getY() + 1, clickedPos.getZ() + 0.5), MobSpawnType.MOB_SUMMONED);
-            if (entityToSpawn != null) {
-                entityToSpawn.setDeltaMovement(0, 0, 0);
+            return InteractionResult.CONSUME;
+        }
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        BlockHitResult blockhitresult = getPlayerPOVHitResult(pLevel, pPlayer, ClipContext.Fluid.SOURCE_ONLY);
+        if (blockhitresult.getType() != HitResult.Type.BLOCK) {
+            return InteractionResultHolder.pass(itemstack);
+        } else if (!(pLevel instanceof ServerLevel)) {
+            return InteractionResultHolder.success(itemstack);
+        } else {
+            BlockPos blockpos = blockhitresult.getBlockPos();
+            if (!(pLevel.getBlockState(blockpos).getBlock() instanceof LiquidBlock)) {
+                return InteractionResultHolder.pass(itemstack);
+            } else if (pLevel.mayInteract(pPlayer, blockpos) && pPlayer.mayUseItemAt(blockpos, blockhitresult.getDirection(), itemstack)) {
+                Entity entity = ModEntities.TARGET_1.get().spawn((ServerLevel)pLevel, itemstack, pPlayer, blockpos, MobSpawnType.SPAWN_EGG, false, false);
+                if (entity == null) {
+                    return InteractionResultHolder.pass(itemstack);
+                } else {
+                    if (!pPlayer.getAbilities().instabuild) {
+                        itemstack.shrink(1);
+                    }
+
+                    pPlayer.awardStat(Stats.ITEM_USED.get(this));
+                    pLevel.gameEvent(pPlayer, GameEvent.ENTITY_PLACE, entity.position());
+                    return InteractionResultHolder.consume(itemstack);
+                }
+            } else {
+                return InteractionResultHolder.fail(itemstack);
             }
         }
-
-        context.getItemInHand().shrink(1);
-        player.swing(InteractionHand.MAIN_HAND, true);
-
-        return InteractionResult.SUCCESS;
     }
 }
