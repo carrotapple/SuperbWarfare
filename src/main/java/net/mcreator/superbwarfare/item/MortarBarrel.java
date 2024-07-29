c918b7f218361530e8a399fbaaf67c108d260db3
17146 <1714673995@qq.com>
2024-07-29 16:54:53 +0800

    删除冗余代码

--- src/main/java/net/mcreator/superbwarfare/item/MortarBarrel.java
+++ src/main/java/net/mcreator/superbwarfare/item/MortarBarrel.java
@@ -1,21 +1,10 @@
 package net.mcreator.superbwarfare.item;
 
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
-
-import java.util.List;
 
 public class MortarBarrel extends Item {
     public MortarBarrel() {
-        super(new Item.Properties().stacksTo(64).fireResistant().rarity(Rarity.COMMON));
+        super(new Item.Properties().fireResistant());
     }
 
-    @Override
-    public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
-        super.appendHoverText(itemstack, world, list, flag);
-    }
 }
