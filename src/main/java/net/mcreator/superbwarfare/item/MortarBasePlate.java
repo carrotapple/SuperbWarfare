6ac6493a5b15506ac63d350d24544dc233487a0c
Atsuihsio <842960157@qq.com>
2024-07-29 17:04:11 +0800

    114514

--- src/main/java/net/mcreator/superbwarfare/item/MortarBasePlate.java
+++ src/main/java/net/mcreator/superbwarfare/item/MortarBasePlate.java
@@ -1,21 +1,9 @@
 package net.mcreator.superbwarfare.item;
 
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
-
-import java.util.List;
 
 public class MortarBasePlate extends Item {
     public MortarBasePlate() {
-        super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
-    }
-
-    @Override
-    public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
-        super.appendHoverText(itemstack, world, list, flag);
+        super(new Item.Properties());
     }
 }
