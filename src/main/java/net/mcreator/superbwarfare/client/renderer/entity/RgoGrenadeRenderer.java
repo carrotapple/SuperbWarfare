0f31cf2ae65de89ee3cae1b5a1f093ff98a97a2d
Atsuihsio <842960157@qq.com>
2024-07-30 23:47:17 +0800

    renderer问题待解决

--- src/main/java/net/mcreator/superbwarfare/client/renderer/entity/RgoGrenadeRenderer.java
+++ src/main/java/net/mcreator/superbwarfare/client/renderer/entity/RgoGrenadeRenderer.java
@@ -3,12 +3,15 @@ package net.mcreator.superbwarfare.client.renderer.entity;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 import net.mcreator.superbwarfare.entity.RgoGrenadeEntity;
 import net.mcreator.superbwarfare.entity.model.RgoGrenadeEntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
@@ -30,6 +33,17 @@ public class RgoGrenadeRenderer extends GeoEntityRenderer<RgoGrenadeEntity> {
 		this.scaleHeight = scale;
 		this.scaleWidth = scale;
 		super.preRender(poseStack, entity, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
+	}
+
+	@Override
+	public void render(RgoGrenadeEntity entityIn, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource bufferIn, int packedLightIn) {
+		VertexConsumer vb = bufferIn.getBuffer(RenderType.eyes(this.getTextureLocation(entityIn)));
+		poseStack.pushPose();
+		poseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(partialTicks, entityIn.yRotO, entityIn.getYRot()) - 90));
+		poseStack.mulPose(Axis.ZP.rotationDegrees(90 + Mth.lerp(partialTicks, entityIn.xRotO, entityIn.getXRot())));
+		model.renderToBuffer(poseStack, vb, packedLightIn, OverlayTexture.NO_OVERLAY, 1, 1, 1, 0.0625f);
+		poseStack.popPose();
+		super.render(entityIn, entityYaw, partialTicks, poseStack, bufferIn, packedLightIn);
 	}
 
 	@Override
