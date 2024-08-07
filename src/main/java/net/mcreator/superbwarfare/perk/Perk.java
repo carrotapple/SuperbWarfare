ccba17fa1f7bf0bc820ead5510c4bb69994be4de
Light_Quanta <zch22248@foxmail.com>
2024-08-07 12:20:37 +0800

    实现Perk和Perk注册

--- src/main/java/net/mcreator/superbwarfare/perk/Perk.java
+++ src/main/java/net/mcreator/superbwarfare/perk/Perk.java
@@ -1,5 +1,19 @@
 package net.mcreator.superbwarfare.perk;
 
-public class Perk {
-    // TODO 完成Perk和PerkItem
+public record Perk(net.mcreator.superbwarfare.perk.Perk.Type type) {
+
+    public enum Type {
+        AMMO(0),
+        FUNCTIONAL(1),
+        DAMAGE(2);
+        private final int slot;
+
+        Type(int slot) {
+            this.slot = slot;
+        }
+
+        public int getSlot() {
+            return slot;
+        }
+    }
 }
