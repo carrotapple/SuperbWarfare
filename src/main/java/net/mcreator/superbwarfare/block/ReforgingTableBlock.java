43dbb4c31d0e0df874e5fac8baa751607ac5e929
Atsuihsio <842960157@qq.com>
2024-08-14 23:53:40 +0800

    设置重铸台的自发光

--- src/main/java/net/mcreator/superbwarfare/block/ReforgingTableBlock.java
+++ src/main/java/net/mcreator/superbwarfare/block/ReforgingTableBlock.java
@@ -33,7 +33,7 @@ public class ReforgingTableBlock extends Block {
     private static final Component CONTAINER_TITLE = Component.translatable("container.superbwarfare.reforging_table");
 
     public ReforgingTableBlock() {
-        super(BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.BASEDRUM).sound(SoundType.STONE).strength(2f));
+        super(BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.BASEDRUM).sound(SoundType.STONE).strength(2f).lightLevel(s -> 4).hasPostProcess((bs, br, bp) -> true).emissiveRendering((bs, br, bp) -> true));
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
@@ -56,6 +56,12 @@ public class ReforgingTableBlock extends Block {
     public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
         return true;
     }
+
+    @Override
+    public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+        return 0;
+    }
+
 
     @Override
     public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
