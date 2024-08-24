c4b71c21170c2656455decfe20031ad4f57f8be7
Atsuihsio <842960157@qq.com>
2024-08-25 02:16:37 +0800

    移除Pose注册

--- src/main/java/net/mcreator/superbwarfare/tools/PoseTool.java
+++ src/main/java/net/mcreator/superbwarfare/tools/PoseTool.java
@@ -1,10 +1,8 @@
 package net.mcreator.superbwarfare.tools;
 
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
@@ -12,13 +10,6 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class PoseTool {
-
-    private static final HumanoidModel.ArmPose TacticalSprintPose = HumanoidModel.ArmPose.create("TacticalSprintPose", false, (model, entity, arm) -> {
-        if (arm == HumanoidArm.RIGHT) {
-            model.rightArm.xRot = 0.2f * model.rightArm.xRot - 155F * Mth.DEG_TO_RAD;
-            model.rightArm.zRot = -5F * Mth.DEG_TO_RAD;
-        }
-    });
 
     public static HumanoidModel.ArmPose pose(LivingEntity entityLiving, InteractionHand hand, ItemStack stack) {
         if (stack.getOrCreateTag().getBoolean("is_empty_reloading")
@@ -28,7 +19,7 @@ public class PoseTool {
             return HumanoidModel.ArmPose.CROSSBOW_CHARGE;
         } else if (entityLiving.isSprinting() && entityLiving.onGround() && entityLiving.getPersistentData().getDouble("noRun") == 0) {
             if (entityLiving.hasEffect(MobEffects.MOVEMENT_SPEED)) {
-                return TacticalSprintPose;
+                return HumanoidModel.ArmPose.THROW_SPEAR;
             } else {
                 return HumanoidModel.ArmPose.CROSSBOW_CHARGE;
             }
