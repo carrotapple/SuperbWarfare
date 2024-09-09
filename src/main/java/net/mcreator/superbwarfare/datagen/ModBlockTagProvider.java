627c7730eba08091470a0394b844809e42ecdbb3
17146 <1714673995@qq.com>
2024-09-09 21:25:33 +0800

    添加方块tag生成器

--- src/main/java/net/mcreator/superbwarfare/datagen/ModBlockTagProvider.java
+++ src/main/java/net/mcreator/superbwarfare/datagen/ModBlockTagProvider.java
@@ -1,8 +1,10 @@
 package net.mcreator.superbwarfare.datagen;
 
 import net.mcreator.superbwarfare.ModUtils;
+import net.mcreator.superbwarfare.init.ModBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -16,6 +18,14 @@ public class ModBlockTagProvider extends BlockTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(BlockTags.NEEDS_IRON_TOOL).add(ModBlocks.GALENA_ORE.get(), ModBlocks.SCHEELITE_ORE.get(),
+                ModBlocks.DEEPSLATE_GALENA_ORE.get(), ModBlocks.DEEPSLATE_SCHEELITE_ORE.get(), ModBlocks.DRAGON_TEETH.get());
+
+        this.tag(BlockTags.MINEABLE_WITH_AXE).add(ModBlocks.BARBED_WIRE.get());
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.GALENA_ORE.get(), ModBlocks.SCHEELITE_ORE.get(),
+                ModBlocks.DEEPSLATE_GALENA_ORE.get(), ModBlocks.DEEPSLATE_SCHEELITE_ORE.get(), ModBlocks.DRAGON_TEETH.get(),
+                ModBlocks.REFORGING_TABLE.get());
+        this.tag(BlockTags.MINEABLE_WITH_SHOVEL).add(ModBlocks.SANDBAG.get());
 
     }
 }
