b09de2873f0f2964ccbcb0da542c221ab8b02664
Atsuihsio <842960157@qq.com>
2024-09-26 11:34:27 +0800

    添加tacz新添加的伤害类型，修复一个翻译错误

--- src/main/java/net/mcreator/superbwarfare/datagen/ModDamageTypeTagProvider.java
+++ src/main/java/net/mcreator/superbwarfare/datagen/ModDamageTypeTagProvider.java
@@ -23,9 +23,11 @@ public class ModDamageTypeTagProvider extends DamageTypeTagsProvider {
     protected void addTags(HolderLookup.Provider pProvider) {
         this.tag(ModTags.DamageTypes.PROJECTILE).add(ModDamageTypes.GUN_FIRE, ModDamageTypes.GUN_FIRE_HEADSHOT,
                         DamageTypes.ARROW, DamageTypes.TRIDENT, DamageTypes.THROWN)
-                .addOptional(new ResourceLocation("tacz", "bullet"));
+                .addOptional(new ResourceLocation("tacz", "bullet"))
+                .addOptional(new ResourceLocation("tacz", "bullet_void"));
         this.tag(ModTags.DamageTypes.PROJECTILE_ABSOLUTE).add(ModDamageTypes.GUN_FIRE_ABSOLUTE, ModDamageTypes.GUN_FIRE_HEADSHOT_ABSOLUTE)
-                .addOptional(new ResourceLocation("tacz", "bullet_ignore_armor"));
+                .addOptional(new ResourceLocation("tacz", "bullet_ignore_armor"))
+                .addOptional(new ResourceLocation("tacz", "bullet_void_ignore_armor"));
     }
 
 }
