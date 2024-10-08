c37a44ff68361cb5fe9d7691f6a3e9ce748b0d44
Atsuihsio <842960157@qq.com>
2024-10-09 03:00:38 +0800

    关闭调试信息

--- src/main/java/net/mcreator/superbwarfare/event/ClientEventHandler.java
+++ src/main/java/net/mcreator/superbwarfare/event/ClientEventHandler.java
@@ -19,7 +19,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
@@ -560,7 +559,7 @@ public class ClientEventHandler {
         }
         recoilY = 0;
 
-        player.displayClientMessage(Component.literal(new java.text.DecimalFormat("##.##").format(recoilY) + " " + new java.text.DecimalFormat("##.#").format(recoilHorizon)), true);
+//        player.displayClientMessage(Component.literal(new java.text.DecimalFormat("##.##").format(recoilY) + " " + new java.text.DecimalFormat("##.#").format(recoilHorizon)), true);
 
         /*
          计算后坐力
