95b385654289a84662d7c7b09ed2e73e41ffd42a
17146 <1714673995@qq.com>
2024-11-02 00:54:53 +0800

    添加彩蛋

--- src/main/java/net/mcreator/superbwarfare/entity/model/ClaymoreModel.java
+++ src/main/java/net/mcreator/superbwarfare/entity/model/ClaymoreModel.java
@@ -5,20 +5,27 @@ import net.mcreator.superbwarfare.entity.ClaymoreEntity;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.GeoModel;
 
+import java.util.UUID;
+
 public class ClaymoreModel extends GeoModel<ClaymoreEntity> {
+
     @Override
     public ResourceLocation getAnimationResource(ClaymoreEntity entity) {
-        return new ResourceLocation(ModUtils.MODID, "animations/claymore.animation.json");
+        return ModUtils.loc("animations/claymore.animation.json");
     }
 
     @Override
     public ResourceLocation getModelResource(ClaymoreEntity entity) {
-        return new ResourceLocation(ModUtils.MODID, "geo/claymore.geo.json");
+        return ModUtils.loc("geo/claymore.geo.json");
     }
 
     @Override
     public ResourceLocation getTextureResource(ClaymoreEntity entity) {
-        return new ResourceLocation(ModUtils.MODID, "textures/entity/claymore.png");
+        UUID uuid = entity.getUUID();
+        if (uuid.getLeastSignificantBits() % 514 == 0) {
+            return ModUtils.loc("textures/entity/claymore_alter.png");
+        }
+        return ModUtils.loc("textures/entity/claymore.png");
     }
 
 }
