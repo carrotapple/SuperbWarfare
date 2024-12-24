a277fb444d13374e5b793cd01e3330284e4b8a07
17146 <1714673995@qq.com>
2024-12-24 16:21:33 +0800

    优化NBT#ExplosionDamage

--- src/main/java/com/atsuishio/superbwarfare/client/tooltip/ClientLauncherImageTooltip.java
+++ src/main/java/com/atsuishio/superbwarfare/client/tooltip/ClientLauncherImageTooltip.java
@@ -2,7 +2,6 @@ package com.atsuishio.superbwarfare.client.tooltip;
 
 import com.atsuishio.superbwarfare.client.tooltip.component.GunImageComponent;
 import com.atsuishio.superbwarfare.tools.GunsTool;
-import com.atsuishio.superbwarfare.tools.ItemNBTTool;
 import com.atsuishio.superbwarfare.tools.TooltipTool;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
@@ -18,7 +17,7 @@ public class ClientLauncherImageTooltip extends ClientGunImageTooltip {
     @Override
     protected Component getDamageComponent() {
         double damage = GunsTool.getGunDoubleTag(stack, "Damage", 0) * TooltipTool.perkDamage(stack);
-        double explosionDamage = ItemNBTTool.getDouble(stack, "ExplosionDamage", 0);
+        double explosionDamage = GunsTool.getGunDoubleTag(stack, "ExplosionDamage", 0);
 
         return Component.translatable("des.superbwarfare.tips.damage").withStyle(ChatFormatting.GRAY)
                 .append(Component.literal("").withStyle(ChatFormatting.RESET))
