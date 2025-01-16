afd7fb550ad2759c23c5dd7af37cee99c6e698c8
17146 <1714673995@qq.com>
2025-01-16 23:08:18 +0800

    修复文本错误

--- src/main/java/com/atsuishio/superbwarfare/compat/clothconfig/client/VehicleControlClothConfig.java
+++ src/main/java/com/atsuishio/superbwarfare/compat/clothconfig/client/VehicleControlClothConfig.java
@@ -10,13 +10,13 @@ public class VehicleControlClothConfig {
 
 
     public static void init(ConfigBuilder root, ConfigEntryBuilder entryBuilder) {
-        ConfigCategory category = root.getOrCreateCategory(Component.translatable("config.superbwarfare.client.vrhicle"));
+        ConfigCategory category = root.getOrCreateCategory(Component.translatable("config.superbwarfare.client.vehicle"));
 
         category.addEntry(entryBuilder
-                .startBooleanToggle(Component.translatable("config.superbwarfare.client.vrhicle.invert_aircraft_control"), VehicleControlConfig.INVERT_AIRCRAFT_CONTROL.get())
+                .startBooleanToggle(Component.translatable("config.superbwarfare.client.vehicle.invert_aircraft_control"), VehicleControlConfig.INVERT_AIRCRAFT_CONTROL.get())
                 .setDefaultValue(true)
                 .setSaveConsumer(VehicleControlConfig.INVERT_AIRCRAFT_CONTROL::set)
-                .setTooltip(Component.translatable("config.superbwarfare.client.vrhicle.left_click_reload.des")).build()
+                .setTooltip(Component.translatable("config.superbwarfare.client.vehicle.left_click_reload.des")).build()
         );
     }
 
