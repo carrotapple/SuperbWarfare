cb958d805b29a9140611135c4d0630f5018bf2fc
17146 <1714673995@qq.com>
2025-03-19 20:23:28 +0800

    调整fast projectile同步机制

--- src/main/java/com/atsuishio/superbwarfare/entity/projectile/FastThrowableProjectile.java
+++ src/main/java/com/atsuishio/superbwarfare/entity/projectile/FastThrowableProjectile.java
@@ -2,7 +2,6 @@ package com.atsuishio.superbwarfare.entity.projectile;
 
 import com.atsuishio.superbwarfare.ModUtils;
 import com.atsuishio.superbwarfare.network.message.ClientMotionSyncMessage;
-import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
@@ -33,13 +32,7 @@ public abstract class FastThrowableProjectile extends ThrowableItemProjectile im
     public void syncMotion() {
         if (this.level().isClientSide) return;
 
-        var motion = this.getDeltaMovement();
-        double length = motion.length();
-        int tickNeeded = 5;
-        if (length > 0) {
-            tickNeeded = (int) Mth.clamp(10D / length, 1, 5);
-        }
-        if (this.tickCount % tickNeeded == 0) {
+        if (this.tickCount % this.getType().updateInterval() == 0) {
             ModUtils.PACKET_HANDLER.send(PacketDistributor.ALL.noArg(), new ClientMotionSyncMessage(this));
         }
     }
