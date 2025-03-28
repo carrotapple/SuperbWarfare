a9db5449e074aea45f7228456330deb205883975
Light_Quanta <zch22248@foxmail.com>
2025-03-29 00:38:39 +0800

    正确保存配置

--- src/main/java/com/atsuishio/superbwarfare/command/ConfigCommand.java
+++ src/main/java/com/atsuishio/superbwarfare/command/ConfigCommand.java
@@ -16,6 +16,7 @@ public class ConfigCommand {
                 .then(Commands.literal("explosionDestroy").requires(s -> s.hasPermission(2)).then(Commands.argument("value", BoolArgumentType.bool()).executes(context -> {
                     var value = BoolArgumentType.getBool(context, "value");
                     ExplosionConfig.EXPLOSION_DESTROY.set(value);
+                    ExplosionConfig.EXPLOSION_DESTROY.save();
 
                     context.getSource().sendSuccess(() -> Component.translatable(value ? "commands.config.explosion_destroy.enabled" : "commands.config.explosion_destroy.disabled"), true);
                     return 0;
