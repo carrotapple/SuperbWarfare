088d7085aa0ab282a7d73ac4dfb67117188cac44
Light_Quanta <zch22248@foxmail.com>
2025-04-18 20:47:46 +0800

    优化PlayerVariableSyncMessage编码

--- src/main/java/com/atsuishio/superbwarfare/network/message/receive/PlayerVariablesSyncMessage.java
+++ src/main/java/com/atsuishio/superbwarfare/network/message/receive/PlayerVariablesSyncMessage.java
@@ -3,7 +3,6 @@ package com.atsuishio.superbwarfare.network.message.receive;
 import com.atsuishio.superbwarfare.network.ModVariables;
 import com.atsuishio.superbwarfare.tools.Ammo;
 import net.minecraft.client.Minecraft;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraftforge.network.NetworkEvent;
 
@@ -14,9 +13,22 @@ public class PlayerVariablesSyncMessage {
     private final ModVariables.PlayerVariables data;
 
     public PlayerVariablesSyncMessage(FriendlyByteBuf buffer) {
+        this.target = buffer.readVarInt();
         this.data = new ModVariables.PlayerVariables();
-        this.data.readNBT(buffer.readNbt());
-        this.target = buffer.readInt();
+
+        while (buffer.isReadable()) {
+            var type = buffer.readByte();
+            switch (type) {
+                case -1 -> this.data.tacticalSprint = buffer.readBoolean();
+                case -2 -> this.data.edit = buffer.readBoolean();
+                default -> {
+                    if (type < Ammo.values().length) {
+                        var ammoType = Ammo.values()[type];
+                        ammoType.set(this.data, buffer.readVarInt());
+                    }
+                }
+            }
+        }
     }
 
     public PlayerVariablesSyncMessage(ModVariables.PlayerVariables data, int entityId) {
@@ -25,8 +37,15 @@ public class PlayerVariablesSyncMessage {
     }
 
     public static void buffer(PlayerVariablesSyncMessage message, FriendlyByteBuf buffer) {
-        buffer.writeNbt((CompoundTag) message.data.writeNBT());
-        buffer.writeInt(message.target);
+        buffer.writeVarInt(message.target);
+        for (var type : Ammo.values()) {
+            buffer.writeByte(type.ordinal());
+            buffer.writeVarInt(type.get(message.data));
+        }
+        buffer.writeByte(-1);
+        buffer.writeBoolean(message.data.tacticalSprint);
+        buffer.writeByte(-2);
+        buffer.writeBoolean(message.data.edit);
     }
 
     public static void handler(PlayerVariablesSyncMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
