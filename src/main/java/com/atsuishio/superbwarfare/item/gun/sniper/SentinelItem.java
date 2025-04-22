5b09a8a23053e326c8a61fa6f285a96b3da9eac2
17146 <1714673995@qq.com>
2025-04-22 23:00:04 +0800

    回滚哨兵耗能方法

--- src/main/java/com/atsuishio/superbwarfare/item/gun/sniper/SentinelItem.java
+++ src/main/java/com/atsuishio/superbwarfare/item/gun/sniper/SentinelItem.java
@@ -20,12 +20,14 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
@@ -172,6 +174,20 @@ public class SentinelItem extends GunItem implements GeoItem {
         return stack.getCapability(ForgeCapabilities.ENERGY)
                 .map(cap -> cap.getEnergyStored() > 0 ? 0.2857142857142857 * data.rawDamage() : 0)
                 .orElse(0D);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int slot, boolean selected) {
+        super.inventoryTick(stack, level, entity, slot, selected);
+
+        stack.getCapability(ForgeCapabilities.ENERGY).ifPresent(
+                energy -> {
+                    int energyStored = energy.getEnergyStored();
+                    if (energyStored > 0) {
+                        energy.extractEnergy(1, false);
+                    }
+                }
+        );
     }
 
     @Override
