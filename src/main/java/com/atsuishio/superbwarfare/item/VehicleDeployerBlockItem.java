498580818c14be417195de3daf6cfc5d2eb922db
17146 <1714673995@qq.com>
2025-05-11 17:21:48 +0800

    添加载具部署器的描述

--- src/main/java/com/atsuishio/superbwarfare/item/VehicleDeployerBlockItem.java
+++ src/main/java/com/atsuishio/superbwarfare/item/VehicleDeployerBlockItem.java
@@ -1,9 +1,11 @@
 package com.atsuishio.superbwarfare.item;
 
 import com.atsuishio.superbwarfare.init.ModBlocks;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
@@ -11,15 +13,15 @@ import org.jetbrains.annotations.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
-// TODO 图标和简介等
 public class VehicleDeployerBlockItem extends BlockItem {
+
     public VehicleDeployerBlockItem() {
-        super(ModBlocks.VEHICLE_DEPLOYER.get(), new Properties().stacksTo(1));
+        super(ModBlocks.VEHICLE_DEPLOYER.get(), new Properties().stacksTo(1).rarity(Rarity.EPIC));
     }
 
     @Override
     @ParametersAreNonnullByDefault
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
-        tooltip.add(Component.literal("test"));
+        tooltip.add(Component.translatable("des.superbwarfare.vehicle_deployer").withStyle(ChatFormatting.GRAY));
     }
 }
