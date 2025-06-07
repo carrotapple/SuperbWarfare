6a2e4a6b1f008d6fddc695657aede9f36ccdaf13
17146 <1714673995@qq.com>
2025-06-08 01:01:45 +0800

    调整overlay

--- src/main/java/com/atsuishio/superbwarfare/client/overlay/ItemRendererFixOverlay.java
+++ src/main/java/com/atsuishio/superbwarfare/client/overlay/ItemRendererFixOverlay.java
@@ -1,14 +1,20 @@
 package com.atsuishio.superbwarfare.client.overlay;
 
 import com.atsuishio.superbwarfare.Mod;
+import com.atsuishio.superbwarfare.item.gun.GunItem;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 
+/**
+ * 这个类的作用是在看不见的地方渲染一个第三人称的武器模型，别管为啥这么干
+ * 反正删了这个绝对会出事
+ */
 @OnlyIn(Dist.CLIENT)
 public class ItemRendererFixOverlay implements IGuiOverlay {
 
@@ -16,14 +22,15 @@ public class ItemRendererFixOverlay implements IGuiOverlay {
 
     @Override
     public void render(ForgeGui gui, GuiGraphics guiGraphics, float partialTick, int screenWidth, int screenHeight) {
-        Player entity = gui.getMinecraft().player;
+        Player player = gui.getMinecraft().player;
+        if (player == null) return;
+        ItemStack stack = player.getMainHandItem();
+        if (!(stack.getItem() instanceof GunItem)) return;
 
-        if (entity != null) {
-            guiGraphics.pose().pushPose();
-            guiGraphics.pose().translate(-1145.0D, 0.0D, 0.0D);
-            gui.getMinecraft().gameRenderer.itemInHandRenderer.renderItem(entity, entity.getMainHandItem(),
-                    ItemDisplayContext.THIRD_PERSON_RIGHT_HAND, false, guiGraphics.pose(), guiGraphics.bufferSource(), 0);
-            guiGraphics.pose().popPose();
-        }
+        guiGraphics.pose().pushPose();
+        guiGraphics.pose().translate(-1145.0D, 0.0D, 0.0D);
+        gui.getMinecraft().gameRenderer.itemInHandRenderer.renderItem(player, stack,
+                ItemDisplayContext.THIRD_PERSON_RIGHT_HAND, false, guiGraphics.pose(), guiGraphics.bufferSource(), 0);
+        guiGraphics.pose().popPose();
     }
 }
