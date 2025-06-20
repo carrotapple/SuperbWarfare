fdb4bae800b4c7af115368da0138cd222eb93b24
Light_Quanta <zch22248@foxmail.com>
2025-06-21 00:48:18 +0800

    实现卓越物品接口物品传输

--- src/main/java/com/atsuishio/superbwarfare/block/SuperbItemInterfaceBlock.java
+++ src/main/java/com/atsuishio/superbwarfare/block/SuperbItemInterfaceBlock.java
@@ -3,6 +3,7 @@ package com.atsuishio.superbwarfare.block;
 import com.atsuishio.superbwarfare.block.entity.SuperbItemInterfaceBlockEntity;
 import com.atsuishio.superbwarfare.init.ModBlockEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -10,6 +11,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -24,34 +26,57 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.ParametersAreNonnullByDefault;
 
 @SuppressWarnings("deprecation")
 public class SuperbItemInterfaceBlock extends BaseEntityBlock {
 
     public static final BooleanProperty ENABLED = BlockStateProperties.ENABLED;
 
+    public static final DirectionProperty FACING = DirectionProperty.create("facing");
+
     public SuperbItemInterfaceBlock() {
-        super(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops().strength(3.0F, 4.8F).sound(SoundType.METAL));
-        this.registerDefaultState(this.stateDefinition.any().setValue(ENABLED, true));
+        this(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).requiresCorrectToolForDrops().strength(3.0F, 4.8F).sound(SoundType.METAL));
     }
 
-    @Nullable
+    public SuperbItemInterfaceBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+
+        this.registerDefaultState(this.stateDefinition.any().setValue(ENABLED, true));
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.DOWN));
+    }
+
     @Override
+    @Nullable
+    @ParametersAreNonnullByDefault
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new SuperbItemInterfaceBlockEntity(pPos, pState);
     }
 
     @Nullable
+    @ParametersAreNonnullByDefault
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
         return pLevel.isClientSide ? null : createTickerHelper(pBlockEntityType, ModBlockEntities.SUPERB_ITEM_INTERFACE.get(), SuperbItemInterfaceBlockEntity::serverTick);
     }
 
+    @Nullable
     @Override
-    public void setPlacedBy(Level pLevel, BlockPos pPos, BlockState pState, LivingEntity pPlacer, ItemStack pStack) {
+    public BlockState getStateForPlacement(@NotNull BlockPlaceContext context) {
+        Direction direction = context.getClickedFace().getOpposite();
+        return this.defaultBlockState()
+                .setValue(FACING, direction)
+                .setValue(ENABLED, true);
+    }
+
+    @Override
+    public void setPlacedBy(@NotNull Level pLevel, @NotNull BlockPos pPos, @NotNull BlockState pState, LivingEntity pPlacer, ItemStack pStack) {
         if (pStack.hasCustomHoverName()) {
             BlockEntity blockentity = pLevel.getBlockEntity(pPos);
             if (blockentity instanceof SuperbItemInterfaceBlockEntity entity) {
@@ -61,6 +86,7 @@ public class SuperbItemInterfaceBlock extends BaseEntityBlock {
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     public void onPlace(BlockState pState, Level pLevel, BlockPos pPos, BlockState pOldState, boolean pIsMoving) {
         if (!pOldState.is(pState.getBlock())) {
             this.checkPoweredState(pLevel, pPos, pState, 2);
@@ -68,7 +94,8 @@ public class SuperbItemInterfaceBlock extends BaseEntityBlock {
     }
 
     @Override
-    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+    @ParametersAreNonnullByDefault
+    public @NotNull InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
         if (pLevel.isClientSide) {
             return InteractionResult.SUCCESS;
         } else {
@@ -82,6 +109,7 @@ public class SuperbItemInterfaceBlock extends BaseEntityBlock {
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     public void neighborChanged(BlockState pState, Level pLevel, BlockPos pPos, Block pBlock, BlockPos pFromPos, boolean pIsMoving) {
         this.checkPoweredState(pLevel, pPos, pState, 4);
     }
@@ -94,6 +122,7 @@ public class SuperbItemInterfaceBlock extends BaseEntityBlock {
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
         if (!pState.is(pNewState.getBlock())) {
             BlockEntity blockentity = pLevel.getBlockEntity(pPos);
@@ -107,26 +136,28 @@ public class SuperbItemInterfaceBlock extends BaseEntityBlock {
     }
 
     @Override
-    public RenderShape getRenderShape(BlockState pState) {
+    public @NotNull RenderShape getRenderShape(@NotNull BlockState pState) {
         return RenderShape.MODEL;
     }
 
     @Override
-    public boolean hasAnalogOutputSignal(BlockState pState) {
+    public boolean hasAnalogOutputSignal(@NotNull BlockState pState) {
         return true;
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     public int getAnalogOutputSignal(BlockState pBlockState, Level pLevel, BlockPos pPos) {
         return AbstractContainerMenu.getRedstoneSignalFromBlockEntity(pLevel.getBlockEntity(pPos));
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(ENABLED);
+        pBuilder.add(ENABLED).add(FACING);
     }
 
     @Override
+    @ParametersAreNonnullByDefault
     public boolean isPathfindable(BlockState pState, BlockGetter pLevel, BlockPos pPos, PathComputationType pType) {
         return false;
     }
