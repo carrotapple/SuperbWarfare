ec64fe0055a8c95007e671df67661cad682a1450
Atsuishio <842960157@qq.com>
2025-06-20 23:45:03 +0800

    添加tacz子弹命中OBB载具时的特效

--- src/main/java/com/atsuishio/superbwarfare/mixins/tacz/EntityUtilMixin.java
+++ src/main/java/com/atsuishio/superbwarfare/mixins/tacz/EntityUtilMixin.java
@@ -1,8 +1,14 @@
 package com.atsuishio.superbwarfare.mixins.tacz;
 
 import com.atsuishio.superbwarfare.entity.OBBEntity;
+import com.atsuishio.superbwarfare.init.ModParticleTypes;
+import com.atsuishio.superbwarfare.init.ModSounds;
 import com.tacz.guns.entity.EntityKineticBullet;
 import com.tacz.guns.util.EntityUtil;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.phys.Vec3;
@@ -13,6 +19,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
+
+import static com.atsuishio.superbwarfare.tools.ParticleTool.sendParticle;
 
 @Mixin(EntityUtil.class)
 public class EntityUtilMixin {
@@ -26,6 +34,12 @@ public class EntityUtilMixin {
                 Optional<Vector3f> optional = obb.clip(startVec.toVector3f(), endVec.toVector3f());
                 if (optional.isPresent()) {
                     cir.setReturnValue(new EntityKineticBullet.EntityResult(entity, new Vec3(optional.get()), false));
+                    if (bulletEntity.level() instanceof ServerLevel serverLevel && bulletEntity.getDeltaMovement().lengthSqr() > 0.01) {
+                        Vec3 hitPos = new Vec3(optional.get());
+                        bulletEntity.level().playSound(null, BlockPos.containing(hitPos), ModSounds.HIT.get(), SoundSource.PLAYERS, 1, 1);
+                        sendParticle(serverLevel, ModParticleTypes.FIRE_STAR.get(), hitPos.x, hitPos.y, hitPos.z, 2, 0, 0, 0, 0.2, false);
+                        sendParticle(serverLevel, ParticleTypes.SMOKE, hitPos.x, hitPos.y, hitPos.z, 2, 0, 0, 0, 0.01, false);
+                    }
                     return;
                 }
             }
